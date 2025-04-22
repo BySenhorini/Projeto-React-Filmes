0018fb33ab1a872d659ac02fd389e3361cb1c1d1
YASMIN SENHORINI DA SILVA <56805860881@info.local>
2025-04-22 16:26:39 -0300

    Projeto React

---
+++ filmes/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return(
+        <header>
+            <img src="" alt=""></img>
+        </header>
+
+    )
+}
+export default Footer;
--- filmes/src/pages/login/Login.jsx
+++ filmes/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import Logo from "../../assets/img/logo.svg";
 import "./Login.css";
+import Botao from "../../components/botao/Botao";
 
 const Login = () => {
     return (
@@ -7,19 +8,20 @@ const Login = () => {
             <div className="banner"></div>
             <section className="section_login">
                 <img src={Logo} alt="Logo do Filmoteca"></img>
-                <form action="">
+                <form action="" className="form_login">
                     <h1>Login</h1>
-                    <div>
-                        <div>
+                    <div className="campos_login">
+                        <div className="campos_input">
                             <label htmlFor="email">E-mail:</label>
                             <input type="email" name="email" placeholder="Digite seu e-mail:" />
                         </div>
-                        <div>
+                        <div className="campos_input">
                             <label htmlFor="senha">Senha:</label>
                             <input type="password" name="senha" placeholder="Digite sua senha:" />
                         </div>
                     </div>
-                </form>
+                    <Botao />
+                </form> 
             </section>
         </main>
     );
